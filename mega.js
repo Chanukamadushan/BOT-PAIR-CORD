a9d85b345ef54d9bc54f4c2e0991433e9acf3bfc
Chanuka <Chanukamc5@gmail.com>
2025-01-19 08:39:45 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,6 +1,6 @@
 const mega = require("megajs");
 const auth = {
-    email: 'Chanukamadushan889@gmail.com',
+    email: 'chanukamadushan889@gmail.com',
     password: 'Chanuka2007#',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
